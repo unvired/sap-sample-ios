27956e0cb155408dc4f12c29e004807ddb54fa0e
Pavithra <pavithra.vp@unvired.io>
2020-04-24 12:48:34 +0530

    updated new metadata, PA functions

--- BE'S/AppConstant.swift
+++ BE'S/AppConstant.swift
@@ -3,14 +3,14 @@ import UIKit
 class AppConstants {
 
 	// App Name
-	static let APP_NAME = "UnviredSAPSample"
+	static let APP_NAME = "UNVIRED_SAP_SAMPLE"
 	
 	// BE Names
 	static let BE_PERSON = "PERSON";
 
 	
 	// PA Function Names
-	static let PA_CREATE_PERSON = "UNVIREDSAPSAMPLE_PA_CREATE_PERSON"; // Create Person
-	static let PA_GET_PERSON = "UNVIREDSAPSAMPLE_PA_GET_PERSON"; // Get Person
+	static let PA_CREATE_PERSON = "UNVIRED_SAP_SAMPLE_PA_CREATE_PERSON"; // Create Person
+	static let PA_GET_PERSON = "UNVIRED_SAP_SAMPLE_PA_GET_PERSON"; // Get Person
 
 }
